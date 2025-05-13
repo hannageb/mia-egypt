b1a2fad95013479ed70d2a2819c09bdfc7d2fd55
Hanna <hannageb@udel.edu>
2025-05-12 22:02:54 -0400

    changed bkgd

--- mia-egypt/src/App.tsx
+++ mia-egypt/src/App.tsx
@@ -5,30 +5,32 @@ import { useNavigate } from 'react-router-dom';
 function App() {
   const navigate = useNavigate();
   return (
-    <div className="nav-bar">
-        <button
-          className="home-button"
-          onClick={() => {
-            navigate('/');
-          } }>Home</button>
-          <button
-            onClick={() => {
-              navigate('/Collections');
-            } }>Collections</button>
-          <button
-            onClick={() => {
-              navigate('/Map');
-            } }>Map</button>
-          <button
-            onClick={() => {
-              navigate('/History');
-            } }>History</button>
-          <button
-          onClick={() => {
-            navigate('/Exhibition');
-          } }>Exhibitions</button>
-          <button>عربي</button>
-      </div>
+    <><div className="nav-bar">
+      <button
+        className="home-button"
+        onClick={() => {
+          navigate('/');
+        } }>Home</button>
+      <button
+        onClick={() => {
+          navigate('/Collections');
+        } }>Collections</button>
+      <button
+        onClick={() => {
+          navigate('/Map');
+        } }>Map</button>
+      <button
+        onClick={() => {
+          navigate('/History');
+        } }>History</button>
+      <button
+        onClick={() => {
+          navigate('/Exhibition');
+        } }>Exhibitions</button>
+      <button>عربي</button>
+    </div>
+    <div className="background">
+    </div></>
   );
 }
 
