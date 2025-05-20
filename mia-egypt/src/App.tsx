0bbeffba94bb0eb84d70f1adbca6dcdf52f67f0b
Hanna <hannageb@udel.edu>
2025-05-19 20:42:28 -0400

    fixed footer

--- mia-egypt/src/App.tsx
+++ mia-egypt/src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import Navigation from './nav-bar/nav-bar';
 import Footer from './nav-bar/footer';
-//import fullLogo from './assets/full_logo.png';
+import fullLogo from './assets/full_logo.png';
 
 
 function App() {
   return (
-    <>
-    <Navigation />
-    {/*<img src={fullLogo} alt="full logo with text" />*/}
-    <Footer/>
-    </>
+    <div className="container">
+      <Navigation />
+      <img src={fullLogo} alt="full logo with text" />
+      <Footer/>
+    </div>
   );
 }
 
