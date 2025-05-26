8308520bcca13ef278906a7c77b55b70935f12ec
Hanna <hannageb@udel.edu>
2025-05-25 22:13:41 -0400

    installed vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import './App.css';
-import Navigation from './nav-bar/nav-bar';
-import Footer from './nav-bar/footer';
-import "./i18n"
-import {useTranslation} from "react-i18next";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const{t} = useTranslation();
+  const [count, setCount] = useState(0)
+
   return (
-    <><Navigation />
-    <div className="h-container">
-        <h2 className="h-header">{t("header")}</h2>
-        <div className='logo-and-text'>
-          <div className="mission">
-            <p>{t("mission")}</p>
-          </div>
-          <div className="logo-container">
-            <div className="logo-bkgd"></div>
-            <div className="full-logo">
-              <img src="/mia-egypt/logos/full-logo.png" alt="logo with text"></img>
-            </div>
-          </div>
-        </div>
-        {/* carousel of some featured items */}
-    </div>
-    <Footer/></>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
-export default App;
+
+export default App
