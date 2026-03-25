11489d58dca5f42c702104afcb27f639959c2930
Hanna <hannageb@udel.edu>
2026-03-24 21:54:22 -0400

    added to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,7 @@
 import Footer from '../nav-bar/footer';
 import './contact.css' 
 
+
 /* 
  * Instagram
  * Facebook
@@ -13,7 +14,20 @@ function Contact() {
     return(
         <>
         <div className="container">
-            
+            <a href="mailto:miainegypt@gmail.com">✉️: miainegypt@gmail.com</a>
+            <a href="tel:(202) 3909930 / 3901520">📞: (202) 3909930 / 3901520</a>
+            <a href="http://maps.google.com/?q=Museum of Islamic Art in Cairo">📍: Port Said, Ghayt Al Adah, Al-Darb Al-Ahmar, Cairo Governorate 11638, Egypt </a>
+            <form style={{padding: "10px"}}>
+                <div className="form-group">
+                    <label>Email Address</label><br/>
+                    <input type="email" className="form-control" placeholder="Enter email"></input>
+                </div>
+                <div className="form-group">
+                    <label>Leave a Message</label><br/>
+                    <textarea id="message" className="form-control" rows={4} cols={50} placeholder="Your message"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary" style={{alignSelf:"left"}}>Send</button>
+            </form>
         </div>
         <Footer /></>
     );
