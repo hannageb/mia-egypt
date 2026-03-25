bdbcc723a4f0ad7c93cfcaf577e336995fb67f68
Hanna <hannageb@udel.edu>
2026-03-24 22:16:58 -0400

    replaced placeholder & fixed button placement on main page

--- src/App.tsx
+++ src/App.tsx
@@ -34,32 +34,35 @@ function App() {
             <img className="full-logo" src="logos/full-logo.png" />
           </div>
         </div>
-        <div className="placeholder">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <div className="about-MIA">
+          <p>The Museum of Islamic Art in Cairo is considered to be the largest Islamic Art museum in the world, housing over 100,000 artifacts covering all branches of Islamic Art from the different periods of Islamic history. Its collection is characterized by both breadth and depth in quantity and quality of coverage, which made it a beacon of Islamic art and civilization throughout its history. It also made it a fountain of knowledge for researchers, historians and visitors from all walks of life, who are interested in delving into the intricacies of Islamic sciences such as Medicine, Engineering and Astronomy. What made this possible is the museum's rich collection of manuscripts and artifacts associated with such fields as Medicine, Surgery, Horticulture and Astronomical instruments like astrolabs, compasses and geographical globes. In the domain of daily life, the collection includes metallic, glass and porcelain utensils, as well as jewelry, weapons, wood and ivory objects, textiles, carpets, etc. The Museum also holds some very rare and unique pieces which demonstrate vividly the level of excellence and exactitude attained by Muslim artisans in their workmanship. Since it's rennovation and expansion in 2014, the Museum has markedly increased the number of displayed artifacts in order to serve its raison d'être while using the state-of-the-Art in interpretive technologies to maximize the visitors' enjoyment experience and their appreciation of the Museum's mission and vision.</p>
         </div>
         {/* carousel of some featured items */}
-        <div className="item-carousel">
           <h1 style={{textAlign: 'center', fontStyle: 'oblique'}}>Featured Items</h1>
-          {page === 1 && ( 
-            <>
-            <div className="carousel-container">
-              {featured.splice(0, 5).map((i: Item) => (
-                <ItemCard key={i.id} item={i}/>
-              ))}
-            </div></>
-          )}
-          {page === 2 && (
-             <div className="carousel-container">
-              {featured.splice(5, 10).map((i: Item) => (
-                <ItemCard key={i.id} item={i}/>
-              ))}</div>
-          )}
-          <div className="page-btn">
-            <button onClick={prevPage} disabled={page === 1}>⬅️</button>
-            <button onClick={nextPage} disabled={page === 5}>➡️</button>
-          </div>
+        <div className="item-carousel">
+            <div className="carousel-main-wrapper">
+              <button onClick={prevPage} disabled={page === 1}>⬅️</button>
+              <div className="carousel-container">
+                {page === 1 && featured.slice(0, 5).map((i: Item) => (
+                  <ItemCard key={i.id} item={i}/>
+                ))}
+                {page === 2 && featured.slice(5, 10).map((i: Item) => (
+                  <ItemCard key={i.id} item={i}/>
+                ))}
+              </div>
+              <button onClick={nextPage} disabled={page === 5}>➡️</button>
+            </div>
         </div>
         {/* carousel of exhibits and workshops */}
+         <div className="exhibit-carousel">
+          <h1 style={{textAlign: 'center', fontStyle: 'oblique'}}>Upcoming Exhibits & Workshops</h1>
+          <div className="carousel-container">
+          </div>
+          <div className="page-btn">
+            <button>⬅️</button>
+            <button>➡️</button>
+          </div>
+        </div>
         </div>
       <Footer /></>
   )
